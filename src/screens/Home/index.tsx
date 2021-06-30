c470ab114755d636f5b1d7eba8237de06353734b
Gabriel Lennon <gabriellennon@Gabriels-MacBook-Air.local>
2021-06-29 22:10:47 -0300

    header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
-
+import {
+  StatusBar,
+} from 'react-native';
 import {
   Container,
-  Title
+  Header,
+  TotalCars,
+  HeaderContent
 } from './styles';
+
+import Logo from '../../assets/logo.svg';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export function Home(){
   return (
     <Container>
-        <Title>Tes</Title>
+        <StatusBar 
+          barStyle="light-content"
+          //barra fica por cima do header e nao descontar como estava (contando o header a partir do staus bar)
+          translucent
+          backgroundColor="transparent"
+        />
+        <Header>
+          <HeaderContent>
+            <Logo 
+              width={RFValue(108)} 
+              height={RFValue(12)} 
+            />
+            <TotalCars>
+              Total de 12 carros
+            </TotalCars>
+          </HeaderContent>
+        </Header>
     </Container>
   );
 }
