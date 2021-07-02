025ea8a1be5440d24b4c95f2fb6bd080f34f2794
Gabriel Lennon <gabriellennon@Gabriels-MacBook-Air.local>
2021-07-01 21:17:12 -0300

    car card

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -11,8 +11,29 @@ import {
 
 import Logo from '../../assets/logo.svg';
 import { RFValue } from 'react-native-responsive-fontsize';
+import { Car } from '../../components/Car';
 
 export function Home(){
+  const carData = {
+    brand: 'audi',
+    name: '5 Coupé',
+    rent: {
+        period: 'ao dia',
+        price: 120
+    },
+    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png'
+  }
+
+  const carDataTwo = {
+    brand: 'audi',
+    name: '5 Coupé',
+    rent: {
+        period: 'ao dia',
+        price: 120
+    },
+    thumbnail: 'https://catalogo.webmotors.com.br/imagens/prod/347842/LAND_ROVER_RANGE_ROVER_VELAR_2.0_P300_GASOLINA_RDYNAMIC_HSE_AUTOMATICO_3478421213210552.png?s=fill&w=440&h=330&q=80&t=true'
+  }
+
   return (
     <Container>
         <StatusBar 
@@ -32,6 +53,8 @@ export function Home(){
             </TotalCars>
           </HeaderContent>
         </Header>
+        <Car  data={carData}/>
+        <Car  data={carDataTwo}/>
     </Container>
   );
 }
