faf341da35fc26d86103a08f29beb2ef9d489f72
Gabriel Lennon <gabriellennon@Gabriels-MacBook-Air.local>
2021-07-06 23:21:57 -0300

    component descricao carro

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+import { Acessory } from '../../components/Acessory';
+
+import SpeedSvg from '../../assets/speed.svg';
+import AccelerationSvg from '../../assets/acceleration.svg';
+import ForceSvg from '../../assets/force.svg';
+import GasolineSvg from '../../assets/gasoline.svg';
+import ExchangeSvg from '../../assets/exchange.svg';
+import PeopleSvg from '../../assets/people.svg';
 
 import {
   Container,
@@ -16,6 +24,7 @@ import {
   Period,
   Price,
   About,
+  Acessories
 } from './styles';
 
 export function CarDetails(){
@@ -40,6 +49,15 @@ export function CarDetails(){
               <Price>R$ 580</Price>
             </Rent>
           </Details>
+
+          <Acessories>
+            <Acessory name="380Km/h" icon={SpeedSvg} />
+            <Acessory name="3.2s" icon={AccelerationSvg} />
+            <Acessory name="800 HP" icon={ForceSvg} />
+            <Acessory name="Gasolina" icon={GasolineSvg} />
+            <Acessory name="Auto" icon={ExchangeSvg} />
+            <Acessory name="2 pessoas" icon={PeopleSvg} />
+          </Acessories>
 
           <About>
             Este é automóvel desportivo. Surgiu do 
