ae1f4e256c8a8527bf76f8a6d9bc118ff6bd08c1
Gabriel Lennon <gabriellennon@Gabriels-MacBook-Air.local>
2021-07-12 22:58:07 -0300

    calendar component

--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -4,6 +4,7 @@ import { StatusBar } from 'react-native';
 
 import { BackButton } from '../../components/BackButton';
 import { Button } from '../../components/Button';
+import { Calendar } from '../../components/Calendar';
 
 import ArroSvg from '../../assets/arrow.svg';
 
@@ -56,7 +57,7 @@ export function Scheduling(){
         </Header>
 
         <Content>
-
+            <Calendar />
         </Content>
 
         <Footer>
